44936917c05436d1a01f004c5160cb1cdf4b7c2b
thomasefbland <thomasefbland@gmail.com>
2022-10-18 11:12:38 -0400

    Update HOST_URL to creatorstudio.vercel.app

--- lib/utils/constants.ts
+++ lib/utils/constants.ts
@@ -1,3 +1,3 @@
 const isProductionEnvironment = process.env.NODE_ENV === 'production';
 
-export const HOST_BASE_URL = !isProductionEnvironment ? 'http://localhost:3000' : 'https://tiktok-creator-studio.vercel.app';
+export const HOST_BASE_URL = !isProductionEnvironment ? 'http://localhost:3000' : 'https://creatorstudio.vercel.app';
