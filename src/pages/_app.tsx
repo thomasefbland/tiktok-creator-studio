c5465eb189ac88369b3afe4288245c38aa22c419
thomasefbland <thomasefbland@gmail.com>
2022-10-25 15:51:30 -0400

    TikTok Login Flow V2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,26 @@
-import '../styles/globals.scss';
-import type { AppProps } from 'next/app';
-import Header from '../components/common/Header';
-import Footer from '../components/common/Footer';
-import ScrollToTopButton from '../components/common/ScrollToTopButton';
-import Head from 'next/head';
+import "../styles/globals.scss";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+import ScrollToTopButton from "../components/common/ScrollToTopButton";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session }>) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Header/>
-      <ScrollToTopButton/>
-      <Component {...pageProps} />
-      <Footer/>
+      <SessionProvider session={session}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Header />
+        <ScrollToTopButton />
+        <Component {...pageProps} />
+        <Footer />
+      </SessionProvider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,35 @@
+import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
-import TikTokProvider from "../../../../lib/nextauth/tiktok-provider";
 
-export const authOptions = {
-    providers: [
-        TikTokProvider({
-            clientId: process.env.NEXTAUTH_TIKTOK_CLIENT_KEY,
-            clientSecret: process.env.NEXTAUTH_TIKTOK_CLIENT_SECRET
-        })
-    ]
-}
+export const authOptions: NextAuthOptions = {
+  providers: [
+    {
+      id: "tiktok",
+      name: "TikTok",
+      type: "oauth",
+      clientId: process.env.TIKTOK_CLIENT_KEY,
+      clientSecret: process.env.TIKTOK_CLIENT_SECRET,
+      authorization: {
+        url: "https://www.tiktok.com/auth/authorize/",
+        params: {
+          scope: "user.info.basic,video.list,video.upload",
+          response_type: "code",
+          client_key: process.env.TIKTOK_CLIENT_KEY,
+        },
+      },
+      token: "https://open-api.tiktok.com/oauth/access_token/",
+      userinfo: "https://open.tiktokapis.com/v2/user/info/",
+      profile(profile) {
+        return {
+          profile: profile,
+          id: profile.id,
+        };
+      },
+      checks: ["state"],
+    },
+  ],
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
